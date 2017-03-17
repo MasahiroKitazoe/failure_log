99358e779831aaac16482e2cadab38060b644004
北添真広 <sas.surfer0jonny@gmail.com>
2017-03-17 16:16:12 +0900

    Installed bootstrap template and ready to use

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311072514) do
+ActiveRecord::Schema.define(version: 20170311124804) do
 
   create_table "failures", force: :cascade do |t|
     t.text     "overview",   limit: 65535
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170311072514) do
     t.string   "unconfirmed_email",      limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "username",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
