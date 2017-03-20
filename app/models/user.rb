3f3397d40653012a4f6ccad9d8d26754c9ce9fd0
北添真広 <sas.surfer0jonny@gmail.com>
2017-03-20 16:21:31 +0900

    Add username registration at sign_up phase and cut email confirmation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :authentication_keys => [:login]
-
-  attr_accessor :login
+         :recoverable, :rememberable, :trackable,
+         :validatable
 
   validates :username,
   uniqueness: { case_sensitive: :false },
   length: { minimum: 4, maximum: 20 }
 
-  def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
-    else
-      where(conditions).first
-    end
-  end
+
 end
