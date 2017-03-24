4d6350ff64ac1e8fc88ccec663c00d1c554cc533
北添真広 <sas.surfer0jonny@gmail.com>
2017-03-24 14:39:25 +0900

    failures#newアクションのビューを開発。フォームの高さと幅を設定。

--- app/controllers/failures_controller.rb
+++ app/controllers/failures_controller.rb
@@ -5,6 +5,9 @@ before_action :authenticate_user!, except: :index
 def index
 end
 
+def show
+end
+
 def new
   @failure = Failure.new
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311124804) do
+ActiveRecord::Schema.define(version: 20170324053449) do
 
   create_table "failures", force: :cascade do |t|
     t.text     "overview",   limit: 65535
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170311124804) do
     t.text     "lesson",     limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "user_id",    limit: 4
   end
 
   create_table "users", force: :cascade do |t|
